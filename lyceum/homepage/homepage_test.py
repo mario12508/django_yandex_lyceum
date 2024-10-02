3474db6312f8c4a7a793c9335e76c7562b714de3
Марсель Хуснуллин <mario12526@yandex.ru>
2024-10-02 22:15:46 +0300

    black fix

--- lyceum/homepage/homepage_test.py
+++ lyceum/homepage/homepage_test.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class HomepageTestCase(TestCase):
     def test_home_page_status_code(self):
         """Тест для проверки, что главная страница возвращает статус 200"""
-        response = self.client.get(reverse('home'))
+        response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
 
     def test_addition(self):
