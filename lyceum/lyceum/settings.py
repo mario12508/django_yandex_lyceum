9e640e7820c12dd193582a229bbd3d2fcaa53834
Марсель Хуснуллин <mario12526@yandex.ru>
2024-10-02 19:32:12 +0300

    second task fix

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
+SECRET_KEY = os.getenv('bugagaga')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
