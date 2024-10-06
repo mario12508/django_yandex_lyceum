76077a7c850fb3ba0c39b58d2ede674bf1fdfafb
Марсель Хуснуллин <mario12526@yandex.ru>
2024-10-06 19:49:20 +0300

    flake8 fixes for 2nd lesson

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.urls import include, path
 
 # from homepage import views
 
