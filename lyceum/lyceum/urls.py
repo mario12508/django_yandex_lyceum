3f610cb3dff8a1d34fd161dd345a7c76c646746d
Марсель Хуснуллин <mario12526@yandex.ru>
2024-10-07 22:44:20 +0300

    fix tests

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path, register_converter
+from django.urls import include, path
 
 
 urlpatterns = [
