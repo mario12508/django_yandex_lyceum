ec6fee530015134ccffe97115c2f8423dcaac30c
Марсель Хуснуллин <mario12526@yandex.ru>
2024-11-01 22:24:53 +0300

    fix some tests errors

--- lyceum/catalog/models.py
+++ lyceum/catalog/models.py
@@ -108,7 +108,7 @@ class Item(DefaultModel):
         verbose_name="теги",
     )
     created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
         ordering = ("name",)
