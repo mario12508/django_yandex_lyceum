c7c16246d6270a25f3eef799dc882c964d93ebe9
Марсель Хуснуллин <mario12526@yandex.ru>
2024-11-11 00:15:20 +0300

    7th lesson first task done

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("ckeditor5/", include("django_ckeditor_5.urls")),
     path("auth/", include("users.urls")),
+    path("auth/", include("django.contrib.auth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
