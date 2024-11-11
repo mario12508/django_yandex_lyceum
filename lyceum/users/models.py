a92f62f45e339d809f3eaf235028ba1ee0e64557
Марсель Хуснуллин <mario12526@yandex.ru>
2024-11-11 23:52:32 +0300

    7th lesson 2nd task processed 7

--- lyceum/users/models.py
+++ lyceum/users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import (
     AbstractUser,
     UserManager as DjangoUserManager,
 )
-from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -15,21 +14,11 @@ class CustomUserManager(DjangoUserManager):
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=False)
+    email = models.EmailField(unique=True, null=True, blank=True)
 
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
-
-    def clean(self):
-        if (
-            CustomUser.objects.filter(email=self.email)
-            .exclude(pk=self.pk)
-            .exists()
-        ):
-            raise ValidationError(
-                "Этот email уже используется другим пользователем.",
-            )
 
     def __str__(self):
         return self.username
